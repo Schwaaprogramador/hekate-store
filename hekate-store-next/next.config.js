9f6b7005209902c0006907a8c38817b8434466f6
Schwaaprogramador <danieltoro.programacion@gmail.com>
2023-02-03 02:24:24 -0500

    Componentes Menu

--- hekate-store-next/next.config.js
+++ hekate-store-next/next.config.js
@@ -3,6 +3,11 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  images: {
+    remotePatterns:[{
+      hostname: "rickandmortyapi.com"
+    }]
+  }
 }
 
 module.exports = nextConfig
